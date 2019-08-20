efc5fd634e4f7e8cdb3abb7a84e6f0197a75d887
anami823 <anami823@gmail.com>
2019-08-20 14:10:25 -0700

    Create routes

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const puppiesCtrl = require('../controllers/api/puppies')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+//Retrieve all puppies
+// router.get('/', puppiesCtrl.index)
+
+//Add a puppy
+// router.get('/puppies', puppiesCtrl.create)
+
+//Show DETAILS for a single puppy by Id
+// router.get('/puppies:id', puppiesCtrl.show)
+
+//Update a single puppy by Id
+// router.get('/puppies:id', puppiesCtrl.update)
+
+//Delete a singel puppy by Id
+// router.get('/puppies:id', puppiesCtrl.delete)
 
 module.exports = router;
